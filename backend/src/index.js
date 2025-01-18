7fe04c53fc21a8376d4cb8d50dc710ccd884895b
Gomolemo Ntlhane <gomolemontlhane@gmail.com>
2025-01-18 18:11:42 +0200

    Feat: Add routes subfolder and auth.route.js file
    - Added 'routes' subfolder to organize application routes
    - Created 'auth.route.js' for handling authentication-related routes
    - Improved project structure for better maintainability and scalability

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
-const express = require=("express")
+import express from "express";
 
-const app = express();
+import authRoutes from ".routes/auth.route.js"
+
+const app =express();
+
+app.use("/api/auth", authRoutes)
 
 app.listen(5001, () => {
      console.log("server is running on port 5001")
