5f81ed00f375347bde87b888eea7f9f94cc0b585
Gomolemo Ntlhane <gomolemontlhane@gmail.com>
2025-01-21 01:34:32 +0200

    `Added token generation and user saving in signup controller`

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt.js";
 
@@ -22,12 +23,22 @@ export const signup = async (req,res) => {
           })
 
           if (newUser) {
+               generateToken(newUser._id, res);
+               await newUser.save();
+
+               res.status(201).json({
+                    _id: newUser._id,
+                    fullName: newUser.fullName,
+                    email: newUser.email,
+                    profilePic: newUser.profilePic,
+               })
           } else {
                res.status(400).json({ message: "Invalied user data"});
           }
 
      } catch (error) {
-     
+          console.log("Error in signup controller", error.message);
+          res.status(500).json({ message: "Internal server error"});
      }
 };
 
