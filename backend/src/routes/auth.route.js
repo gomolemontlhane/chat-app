8721a08d6525e8a5ec93e88c083512f71a2e8e64
Gomolemo Ntlhane <gomolemontlhane@gmail.com>
2025-01-20 17:25:23 +0200

    modified files

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,7 @@
-import express from "express"
+import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
 
-const router =  express.Router()
+const router =  express.Router();
 
 router.post("/signup", signup); 
 router.post("/login", login);
