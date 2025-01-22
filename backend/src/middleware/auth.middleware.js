2114a5562f589a395de8c40d9332ba97b96be18c
Gomolemo Ntlhane <gomolemontlhane@gmail.com>
2025-01-23 01:33:26 +0200

    `Added checkAuth controller and updated routes to include checkAuth endpoint`

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,7 +3,7 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
      try {
-          const token =req.cookie.jwt;
+          const token = req.cookies.jwt;
 
           if (!token) {
                return res.status(401).json({ message: "Unauthorized - No Token Provided" });
@@ -15,16 +15,18 @@ export const protectRoute = async (req, res, next) => {
                return res.status(401).json({ message: "Unauthorized - Invalid Token" });
           }
           
-          const user = await User.findById(decoded.userId).select("-passwprd");
+          const user = await User.findById(decoded.userId).select("-password");
 
           if (!user) {
-               return res.status(401).json({ message: "User Not Found" });
+               return res.status(404).json({ message: "User Not Found" });
           }
+          
           req.user = user;
+          
           next();
 
      } catch (error) {
-          console.log("Error in protectRoute middleware", error.message);
+          console.log("Error in protectRoute middleware: ", error.message);
           res.status(500).json({ message: "Internal Server Error" });
      }
 };
