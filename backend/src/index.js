ae905cd5e403734349d90e76157ef9b4e5980c35
Ncediso Nkambule <ncediso@gmail.com>
2025-01-23 17:50:43 +0200

    Repostatus check

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
      console.log("server is running on port:" + PORT);
